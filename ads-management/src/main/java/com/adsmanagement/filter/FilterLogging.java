0363234846d5cbb9c05fcca0c54eec138aee0089
LamDDT <108422896+LamDDT@users.noreply.github.com>
2024-01-12 20:06:22 +0700

    Update Logging (Report, User, Surface,...)

--- ads-management/src/main/java/com/adsmanagement/filter/FilterLogging.java
+++ ads-management/src/main/java/com/adsmanagement/filter/FilterLogging.java
@@ -1,6 +1,10 @@
 package com.adsmanagement.filter;
 
+import com.adsmanagement.reports.ReportService;
 import com.adsmanagement.spaces.SpaceService;
+import com.adsmanagement.surfaces.SurfaceService;
+import com.adsmanagement.users.UserService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,8 +21,11 @@ import java.io.IOException;
 @Component
 public class FilterLogging extends OncePerRequestFilter {
     private static final Logger logSpaces = LoggerFactory.getLogger(SpaceService.class);
+    private static final Logger logSurfaces = LoggerFactory.getLogger(SurfaceService.class);
 
+    private static final Logger logReports = LoggerFactory.getLogger(ReportService.class);
 
+    private static final Logger logUsers = LoggerFactory.getLogger(UserService.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -31,8 +38,21 @@ public class FilterLogging extends OncePerRequestFilter {
         String responseBody = getValueAsString(cachingResponseWrapper.getContentAsByteArray(),cachingResponseWrapper.getCharacterEncoding());
 
         if(request.getRequestURI().startsWith("/api/")){
+
             if(request.getRequestURI().startsWith("/api/v1/spaces")){
                 logSpaces.info("Filter: METHOD = {}; REQUEST URI = {}; REQUEST BODY = {}; RESPONSE CODE = {}; RESPONSE BODY = {}",
+                        request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody);
+            }
+            else if(request.getRequestURI().startsWith("/api/v1/surfaces")){
+                logSurfaces.info("Filter: METHOD = {}; REQUEST URI = {}; REQUEST BODY = {}; RESPONSE CODE = {}; RESPONSE BODY = {}",
+                        request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody);
+            }
+            else if(request.getRequestURI().startsWith("/api/v1/reports")){
+                logReports.info("Filter: METHOD = {}; REQUEST URI = {}; REQUEST BODY = {}; RESPONSE CODE = {}; RESPONSE BODY = {}",
+                        request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody);
+            }
+            else if(request.getRequestURI().startsWith("/api/v1/users")){
+                logUsers.info("Filter: METHOD = {}; REQUEST URI = {}; REQUEST BODY = {}; RESPONSE CODE = {}; RESPONSE BODY = {}",
                         request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody);
             }
         }

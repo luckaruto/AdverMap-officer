2f7dbbc5f72784d4ce348de7f7412cc21197020d
LamDDT <108422896+LamDDT@users.noreply.github.com>
2024-01-13 14:28:26 +0700

    Update constants.jsx

--- advermap-ui/src/services/apis/constants.jsx
+++ advermap-ui/src/services/apis/constants.jsx
@@ -9,6 +9,9 @@ export const API={
 	SURFACE:"/api/v1/surfaces",
 	SPACE:"/api/v1/spaces",
 	REPORT:"/api/v1/reports",
+	CITY:"/api/v1/cities",
+	WARD:"/api/v1/wards",
+	DISTRICT:"/api/v2/districts"
 }
 export const testToken=`eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiRElTVFJJQ1RfQURNSU4iLCJzdWIiOiJkaXN0cmljdGFkbWluQGdtYWlsLmNvbSIsImlhdCI6MTcwNDc5NDcxMSwiZXhwIjoxNzA0Nzk2NTExfQ.APL5p0IeVByd9e-i7AQ1bK1jH57hzTNGfRu_tb_onCk`
 
